a237cd85d9cedb43dae01f7afabd1d8f68622fdd
Golla Lakshmi Srinivas <lsrinivas0768@gmail.com>
2026-08-15 20:45:06 +0530

    Time: 1455 ms (84.27%) | Memory: 30.5 MB (97.33%) - LeetSync

--- 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.py
+++ 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.py
@@ -16,33 +16,48 @@ class Solution(object):
 
 # ----------Sorted list + Binary Search + insertion--------------
 
-        n = len(nums)
-        res = [0] * n
+# Time : Binary search: O(log n)
+#     Insertion:     O(n)
+#     Total:         O(n²)
+# Space : O(n)
 
-        # Stores elements to the right in sorted order
-        sorted_list = []
+        # n = len(nums)
+        # res = [0] * n
 
-        # Traverse from right to left
-        for i in range(n - 1, -1, -1):
+        # # Stores elements to the right in sorted order
+        # sorted_list = []
 
-            # Binary search for the first position
-            # where nums[i] can be inserted
-            left = 0
-            right = len(sorted_list)
+        # # Traverse from right to left
+        # for i in range(n - 1, -1, -1):
 
-            while left < right:
-                mid = (left + right) // 2
+        #     # Binary search for the first position
+        #     # where nums[i] can be inserted
+        #     left = 0
+        #     right = len(sorted_list)
 
-                if sorted_list[mid] < nums[i]:
-                    left = mid + 1
-                else:
-                    right = mid
+        #     while left < right:
+        #         mid = (left + right) // 2
 
-            # 'left' = number of elements smaller than nums[i]
-            res[i] = left
+        #         if sorted_list[mid] < nums[i]:
+        #             left = mid + 1
+        #         else:
+        #             right = mid
 
-            # Insert nums[i] while keeping sorted_list sorted
-            sorted_list.insert(left, nums[i])
+        #     # 'left' = number of elements smaller than nums[i]
+        #     res[i] = left
 
+        #     # Insert nums[i] while keeping sorted_list sorted
+        #     sorted_list.insert(left, nums[i])
+
+        # return res
+
+
+# ----------sortedList just for learning ------------------
+        res = []
+        sorted_nums = SortedList(nums)
+        for e in nums:
+            idx = sorted_nums.index(e)
+            res.append(idx)
+            sorted_nums.remove(e)
         return res
         
