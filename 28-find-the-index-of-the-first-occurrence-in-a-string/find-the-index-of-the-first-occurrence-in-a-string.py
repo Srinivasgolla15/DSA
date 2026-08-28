7afaa1357eab4baf700ee7e1b132e8bfafff43db
Golla Lakshmi Srinivas <lsrinivas0768@gmail.com>
2026-08-28 18:33:19 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (6.14%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -5,22 +5,90 @@ class Solution(object):
         :type needle: str
         :rtype: int
         """
-        n =len(needle)
-        i=0
-        k=0
-     
-        if haystack == needle:
+
+# -----------BRUTE FORCE --------------------
+        # n =len(needle)
+        # i=0
+        # k=0
+        # if haystack == needle:
+        #     return 0
+        # while i<=len(haystack)-n:
+        #     count = 0
+        #     for j in range(i,i+n):        
+        #         if haystack[j] == needle[k]:
+        #             count+=1
+        #         k+=1
+        #     if count == n:
+        #         return i  
+        #     i+=1    
+        #     k=0    
+        # return -1  
+# Time: O((N - M + 1) * M) → O(N * M)
+# N = len(haystack), M = len(needle)
+
+# Space: O(1)  
+
+
+# ---------------SHORTER VERSION OF BRUTEFORCE -----------------
+        # for i in range(len(haystack) - len(needle) + 1):
+        #     if haystack[i:i + len(needle)] == needle:
+        #         return i
+        # return -1
+
+# Time: O((N - M + 1) * M) → O(N * M)
+# N = len(haystack), M = len(needle)
+
+# Space: O(1) 
+
+
+
+# --------------- KMP (Knuth–Morris–Pratt) String Matching using LPS (Longest Prefix Suffix) array --------------------------
+
+        if needle == "":
             return 0
-        while i<=len(haystack)-n:
-            count = 0
-            for j in range(i,i+n):        
-                if haystack[j] == needle[k]:
-                    count+=1
-                k+=1
-            if count == n:
-                return i  
-            i+=1    
-            k=0    
-           
-   
-        return -1    
+
+        lps = [0] * len(needle)
+
+        prevLPS, i = 0, 1
+
+        while i < len(needle):
+            if needle[i] == needle[prevLPS]:
+                lps[i] = prevLPS + 1
+                prevLPS += 1
+                i += 1
+
+            elif prevLPS == 0:
+                lps[i] = 0
+                i += 1
+
+            else:
+                prevLPS = lps[prevLPS - 1]
+
+        i = 0  # ptr for haystack
+        j = 0  # ptr for needle
+
+        while i < len(haystack):
+            if haystack[i] == needle[j]:
+                i, j = i + 1, j + 1
+
+            else:
+                if j == 0:
+                    i += 1
+                else:
+                    j = lps[j - 1]
+
+            if j == len(needle):
+                return i - len(needle)
+
+        return -1
+
+# Time Complexity: O(N + M)
+# N = length of haystack
+# M = length of needle
+# Building LPS → O(M)
+# Searching using KMP → O(N)
+
+# Space Complexity: O(M)
+# LPS array stores M elements
+
+
