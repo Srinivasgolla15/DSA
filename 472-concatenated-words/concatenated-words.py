6841e963bc72cd8b1e919f8d7942e5c1c954e8fc
Golla Lakshmi Srinivas <lsrinivas0768@gmail.com>
2026-09-06 11:57:39 +0530

    Time: 185 ms (66.50%) | Memory: 26.9 MB (37.50%) - LeetSync

---
+++ 472-concatenated-words/concatenated-words.py
@@ -0,0 +1,76 @@
+class Solution(object):
+    def findAllConcatenatedWordsInADict(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+# ---------------DP memoized split code ---------------------
+
+# Let:
+# n = number of words
+# L = maximum length of a word
+#
+# For one word:
+# There are O(L) possible starting positions.
+# For each position, we try O(L) possible ending positions.
+#
+# Time for one word: O(L²)
+#
+# Checking "part in wordSet" is O(L) in Python in the worst case
+# because 'part' is a substring of length up to L.
+#
+# Therefore, worst-case time for one word: O(L³)
+#
+# For all n words:
+# Time: O(n * L³)
+#
+# Space:
+# wordSet = O(n)
+# memo = O(L²) because key = (start, count)
+# Recursion stack = O(L)
+#
+# Total Space: O(n + L²)
+
+        wordSet = set(words)
+        memo = {}
+
+        def canForm(word, start, count):
+
+            # We reached the end
+            if start == len(word):
+                return count >= 2
+
+            # Already solved this state
+            key = (start, count)
+
+            if key in memo:
+                return memo[key]
+
+            # Try every possible next word
+            for end in range(start + 1, len(word) + 1):
+
+                part = word[start:end]
+
+                if part in wordSet:
+                    if canForm(word, end, count + 1):
+                        memo[key] = True
+                        return True
+
+            memo[key] = False
+            return False
+
+        ans = []
+
+        for word in words:
+
+            # Don't let the word use itself as one piece
+            wordSet.remove(word)
+
+            memo = {}
+
+            if canForm(word, 0, 0):
+                ans.append(word)
+
+            wordSet.add(word)
+
+        return ans
